0fe6ad0b6ae3ae473a9853cac9b5b3886eb8cebb
golf5885 <golf5885@yonsei.ac.kr>
2023-06-22 22:07:55 +0900

    asdf

--- src/pages/Character/Character.js
+++ src/pages/Character/Character.js
@@ -8,9 +8,23 @@ import test from '../../icons/test.gif';
 
 function Character() {
   const characters = [
-    { name: '민성우', style: '먹는 것에 돈을 아끼지 않는다.', img_url: 'https://w7.pngwing.com/pngs/390/806/png-transparent-rilakkuma-kakaotalk-kakao-friends-south-korea-kakaofriends-sticker-desktop-wallpaper-snout-thumbnail.png' },
-    { name: '박유찬', style: "ESFP 굉장히 즉흥적이고 P 성향을 가진 20대 연세대 학생. 유럽 여행이 처음이라서 굉장히 설레함. ", img_url: 'https://e7.pngegg.com/pngimages/982/1017/png-clipart-kakaotalk-kakao-friends-sticker-line-ryan-smiley-sticker.png' },
-    { name: '서우석', style: '현지인들과 어울리기를 좋아한다.', img_url: test },
+    { name: '민성우',
+    style: '오기 전 철저한 계획을 세웠으며 역사와 문화에 대한 깊은 관심을 가지고 있다. 파리의 아름다운 건축물과 예술 작품을 즐기며, 역사적인 장소와 음식을 탐험하며 독특한 경험을 추구한다.', 
+    mbti : "ENTJ", 
+    money: "Old and Rich",
+    img_url: 'https://w7.pngwing.com/pngs/390/806/png-transparent-rilakkuma-kakaotalk-kakao-friends-south-korea-kakaofriends-sticker-desktop-wallpaper-snout-thumbnail.png' },
+
+    { name: '박유찬',
+    style: "즉흥적이고 P 성향을 가진 20대 학생이다. 유럽 여행이 처음이라서 굉장히 설레함. 혼자 배낭여행중인 만큼, 자금이 여유롭지 못하고 유명 관광지만 빠르게 도장 찍고 싶어 한다.",
+    mbti : "ESTP",
+    money: "Young and Poor",
+    img_url: 'https://e7.pngegg.com/pngimages/982/1017/png-clipart-kakaotalk-kakao-friends-sticker-line-ryan-smiley-sticker.png' },
+
+    { name: '서우석',
+    style: '신중하고 현실적인 성향을 가진 여행자이다. 가족과 함께 방문할 예정이며, 안정적인 여행을 선호한다. 파리의 아름다운 전망을 즐기고, 가족과 시간을 즐기며 휴식과 재충전하고 싶어한다.',
+    mbti : "ISFJ",
+    money: "Young and Rich",
+    img_url: test },
   ];
 
   return (
@@ -24,6 +38,8 @@ function Character() {
                 <div className="character-info">
                   <h2>{character.name}</h2>
                   <p>{character.style}</p>
+                  <p>MBTI : {character.mbti}</p>
+                  <p>MONEY : {character.money}</p>
                 </div>
               </div>
             </Link>
